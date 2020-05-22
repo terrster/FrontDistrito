0586ee705a977adc480483bd4cd9fd046649aa02
ErikVillarreal-bit <eriksamuel76@gmail.com>
2020-05-21 21:48:14 -0500

    comercialInfo

--- src/components/Appliance/ComercialInfo.js
+++ src/components/Appliance/ComercialInfo.js
@@ -1,6 +1,6 @@
 import Title from '../Generic/Title';
 import { connect } from 'react-redux';
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { execToast } from '../../utils/ToastUtils';
 import ComercialInfoForm from '../../forms/ComercialInfoForm';
 import { variablesManager } from '../Manager/VariablesManager';
@@ -11,162 +11,62 @@ import Steps from './Steps';
 import CustomModal from '../Generic/CustomModal';
 
 
-class ComercialInfo extends Component {
-	constructor(props) {
-		super(props);
-		this.state = { 
-			modalStatus: false,
-			loading: true,
-			dataApp: null,
-			initialValues: {}
-		};
-	}
-	
-	async componentDidMount() {
-		const dataApp = {
-			getAppliance: null,
-			status: true,
-			idAmount: null, 
-			idComercialInfo: "", 
-			idGeneralInfo: null, 
-			idDocuments: {
-				status: false
-			}, 
-		};
-		this.setState({ loading: false, dataApp });
-		if (!this.props.toast) {
-			execToast('second');
-			this.props.updateToast('second');
-		}
-		
-		const { data: {comercial} } = await axios.get(`api/info-comercial/5ec54b2647c5d310409aec1f`, {
-			headers: {
-				token: sessionStorage.getItem("token")
+const ComercialInfo = (props) => {
+
+	const [user, setUser] = useState(JSON.parse(sessionStorage.getItem('user')));
+	const [initialValues, setInitialValues] = useState({});
+
+	const onFormSubmit = async (data) => {
+		const user = JSON.parse(sessionStorage.getItem('user'));
+		console.log(user);
+		const id = user._id;
+		const idClient = user.idClient[user.idClient.length - 1];
+		//--
+		if(idClient.appliance.length > 0){
+			const appliance = idClient.appliance[idClient.appliance.length - 1];
+			if(appliance.idComercialInfo.length > 0){
+				const comercial = appliance.idComercialInfo[appliance.idComercialInfo.length - 1]
+				const id = comercial._id;
+				console.log(id);
+				/* try {
+					const res = await axios.put(`api/info-comercial/${id}`,data,{
+						headers:{
+							token: sessionStorage.getItem('token')
+						}
+					});
+					// Se actualiza el usuario en session storage
+					await sessionStorage.setItem('user', JSON.stringify(res.data.user));
+					// Redireccionar al siguiente formulario
+					history.push(`/informacion-general/${user._id}`);
+				} catch (error) {
+					console.log("Error de servicio",error);
+				} */
 			}
-		});
-		
-		this.setState({ ...comercial });
-		
-	}
-	
-	getTable = () => {
-		let appliance =  {
-			getAppliance: undefined,
-			status: true,
-			idAmount: null,
-			idComercialInfo: '1', 
-			idGeneralInfo: null, 
-			idDocuments: {
-				status: false
-			}, 
-		};
-		const error= false;
-		if (error) {
-			window.location.reload();
-			return false;
 		}
-		if (this.state.loading) {
-			
-			return false;
-		}
-		if (
-			this.props.applianceComercialInfo &&
-			this.state.dataApp.getAppliance.idComercialInfo === null
-		) {
-			/* refetch().then(_ => {}); */
-			
-			return false;
-		}
-		let currentComercialInfo =
-		appliance.getAppliance === undefined ||
-		appliance.idComercialInfo === null
-				? []
-				: appliance.idComercialInfo;
-		let isUpdate = currentComercialInfo.length === 0 ? false : true;
-		let typeForm = isUpdate ? 'update' : 'create';
-		currentComercialInfo =
-			currentComercialInfo.length === 0
-				? error/* new ComercialInfoEntity()  */
-				:  error/* new ComercialInfoEntity().fromGraphQlObject(
-						currentComercialInfo
-				  ); */
-		if (isUpdate) {
-			let cAddress = currentComercialInfo.address;
-			for (const k in cAddress) {
-				if (k !== 'id') {
-					currentComercialInfo[k] = cAddress[k];
-				}
-			}
-			currentComercialInfo.warranty = currentComercialInfo.warranty
-				? '1'
-				: '0';
-			currentComercialInfo.terminal = currentComercialInfo.terminal
-				? '1'
-				: '0';
-		}else{
-			return (
-			
-				<ComercialInfoForm
-					onSubmit={d => {
-						this.onFormSubmit(d, typeForm, currentComercialInfo);
-					}}
-					data={currentComercialInfo}
-					intialValues={this.state.initialValues !== {} ? this.state.initialValues : null}
-				></ComercialInfoForm>
-			);
-		}
-		
 	}
 
-	async getData(){
-		// fetch (Peticion a la API para obtener los datos de la solicitud)
-		try{
-			const infoPost = await axios.get('api/info/comercial',{
-				headers: {
-					token: sessionStorage.getItem('token')
-				}
-			});
-			console.log("Respuesta del back Get", infoPost);
-		}catch(error){
-			console.log("Error del get",error)
-		}
-
-		
-	}
-
-	onFormSubmit = async (data, typeForm, beforeData) => {
-		try{
-			const infoPost = await axios.post('api/info/comercial/store',data,{
-				headers: {
-					token: sessionStorage.getItem('token')
-				}
-			});
-			console.log("Respuesta del back Post", infoPost);
-			console.log("Peticion hecha");
-		}catch(error){
-			console.log("Error del back",error)
-		}
-		this.getData()
-	};
-
-	render() {
-		return (
-			<div className="container mt-3">
-				<Steps />
-				<div className="text-center mb-2">
-					<Title
-						title="Datos del negocio"
-						className="coolvetica fz42 blackBlue"
-					/>
-				</div>
-				<CustomModal
-					modalName="comercialInfoError"
-					message="Error al subir los archivos. Favor de regresar a la pantalla de inicio y continúa tu solicitud."
+	return (
+		<div className="container mt-3">
+			<Steps />
+			<div className="text-center mb-2">
+				<Title
+					title="Datos del negocio"
+					className="coolvetica fz42 blackBlue"
 				/>
-					{this.getTable()}
 			</div>
-		);
-	}
+			<CustomModal
+				modalName="comercialInfoError"
+				message="Error al subir los archivos. Favor de regresar a la pantalla de inicio y continúa tu solicitud."
+			/>
+			<ComercialInfoForm
+				/* onSubmit={data => {
+					onFormSubmit(data);
+				}} */
+				data={props.currentComercialInfo}
+				intialValues={initialValues} 
+			></ComercialInfoForm>
+		</div>
+	);
 }
 
 /* ComercialInfo = compose(
