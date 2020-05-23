c558fa9c23c00bd44083d52b3d129cbf6a21f63a
ErikVillarreal-bit <eriksamuel76@gmail.com>
2020-05-23 14:18:29 -0500

    loader

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../css/loader.css'
+
+
+const Loader = () => {
+	return ( 
+		<div className="loader-container">
+			<div className="lds-ripple">
+				<div/><div/>
+			</div>
+		</div>
+	);
+}
+ 
+export default Loader;
