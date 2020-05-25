c9eb6989065c50d5f6144038a65365079c67d54e
wayasmiguel <wayasmiguel@outlook.com>
2020-05-24 21:24:16 -0500

    configuraciones aws

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,7 +3,9 @@ import axios from 'axios';
 const Axios = axios.create({
     baseURL: `${process.env.REACT_APP_BACKEND}`,
     headers: {
-        token: `${sessionStorage.getItem('token')}`
+        token: `${sessionStorage.getItem('token')}`,
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
     }
 });
 
