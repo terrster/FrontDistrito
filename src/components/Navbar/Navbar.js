df6766bb38a589a501b4eb263cbed35b623a3ea8
ErikVillarreal-bit <eriksamuel76@gmail.com>
2020-05-31 23:23:22 -0500

    FEAT-headerfooter

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -9,8 +9,8 @@ const NavBar = props => {
 	const [isOpen, setIsOpen] = useState(false);
 	const history = useHistory();
 	const close = () => setIsOpen(false);
-	let classDefault = "brandonMed text-center fz16 gray50 heigth-45 nav-btn d-flex hvr-underline-from-left";
-	let classDefaultLink = "brandonMed text-center fz16 gray50 heigth-45 nav-btn d-flex";
+	let classDefault = "text-center fz16 gray50 heigth-45 nav-btn d-flex hvr-underline-from-left";
+	let classDefaultLink = "text-center fz16 gray50 heigth-45 nav-btn d-flex";
 	return (
 		<div className="pos-f-t">
 		 <Navbar collapseOnSelect className="nav-bar" expand="lg" bg="light" expanded={isOpen}>
@@ -19,12 +19,12 @@ const NavBar = props => {
 		 		{isAuthenticated() === false && 
 		 			<Navbar.Collapse>
 		 				<Nav className="ml-auto ">
-		 					<Nav.Link  onClick={close} href="/#simulador" id="ymb-dp-nav-simulator" className="hvr-underline-from-left brandonMed text-center fz16 blackNav nav-btn heigth-45 solicita-butto" style={{ fontFamily: "MetropolisRegular"}}>Simulador</Nav.Link>
-		 					<Nav.Link  onClick={close} href="/#about" id="ymb-dp-nav-about" className="hvr-underline-from-left brandonMed text-center fz16 blackNav nav-btn heigth-45 solicita-butto" style={{ fontFamily: "MetropolisRegular" }}>Nosotros</Nav.Link>
-		 					<Nav.Link  onClick={close} href="/#howWorks" id="ymb-dp-nav-howWorks" className="hvr-underline-from-left brandonMed text-center fz16 blackNav nav-btn heigth-45 solicita-butto" style={{ fontFamily: "MetropolisRegular" }}>¿Cómo funciona?</Nav.Link>
-		 					<Nav.Link  onClick={close} href="https://distritopyme.com.mx/" id="ymb-dp-nav-howWorks" target="_blank" className="hvr-underline-from-left brandonMed text-center fz16 blackNav nav-btn heigth-45 solicita-butto" style={{ fontFamily: "MetropolisRegular" }}>Blog</Nav.Link>
-		 					<Nav.Link  onClick={close} as={NavLink} to="/login" id="ymb-dp-nav-register" className="hvr-underline-from-left brandonMed text-center fz16 blackNav nav-btn heigth-45 solicita-butto" style={{ fontFamily: "MetropolisRegular" }}>Inicia sesión</Nav.Link>
-		 					<Nav.Link onClick={close} as={NavLink} to="/registrate" id="ymb-dp-nav-login" className="brandonMed text-center fz16 white nav-btn primary heigth-45 w-360 ml-auto mr-auto solicita-button" style={{ fontFamily: "MetropolisRegular" , textDecoration: "none"}}>Solicita tu crédito</Nav.Link>
+		 					<Nav.Link  onClick={close} href="/#simulador" id="ymb-dp-nav-simulator" className="hvr-underline-from-left  text-center fz16 blackNav nav-btn heigth-45 solicita-butto" style={{ fontFamily: "MetropolisRegular"}}>Simulador</Nav.Link>
+		 					<Nav.Link  onClick={close} href="/#about" id="ymb-dp-nav-about" className="hvr-underline-from-left  text-center fz16 blackNav nav-btn heigth-45 solicita-butto" style={{ fontFamily: "MetropolisRegular" }}>Nosotros</Nav.Link>
+		 					<Nav.Link  onClick={close} href="/#howWorks" id="ymb-dp-nav-howWorks" className="hvr-underline-from-left  text-center fz16 blackNav nav-btn heigth-45 solicita-butto" style={{ fontFamily: "MetropolisRegular" }}>¿Cómo funciona?</Nav.Link>
+		 					<Nav.Link  onClick={close} href="https://distritopyme.com.mx/" id="ymb-dp-nav-howWorks" target="_blank" className="hvr-underline-from-left  text-center fz16 blackNav nav-btn heigth-45 solicita-butto" style={{ fontFamily: "MetropolisRegular" }}>Blog</Nav.Link>
+		 					<Nav.Link  onClick={close} as={NavLink} to="/login" id="ymb-dp-nav-register" className="hvr-underline-from-left  text-center fz16 blackNav nav-btn heigth-45 solicita-butto" style={{ fontFamily: "MetropolisRegular" }}>Inicia sesión</Nav.Link>
+		 					<Nav.Link onClick={close} as={NavLink} to="/registrate" id="ymb-dp-nav-login" className=" text-center fz16 white nav-btn primary heigth-45 w-360 ml-auto mr-auto solicita-button" style={{ fontFamily: "MetropolisRegular" , textDecoration: "none"}}>Solicita tu crédito</Nav.Link>
 		 				</Nav>
 		 			</Navbar.Collapse>
 		 		}
@@ -34,7 +34,7 @@ const NavBar = props => {
 		 				<Link onClick={close} to="/credito/solicitud/:idAppliance" className={ (history.location.pathname === '/credito') ? classDefaultLink +' nav_bar_active': classDefault}>Mi crédito</Link>
 		 				<Link onClick={close} to="/historial" className={ (history.location.pathname === '/historial') ? classDefaultLink+' nav_bar_active': classDefault}>Historial</Link>
 		 				<Nav.Link onClick={close} href="/home" className={ (history.location.pathname === '/home') ? classDefaultLink+' nav_bar_active': classDefault}>Mi cuenta</Nav.Link>
-		 				<Nav.Link onClick={close} href="/" className="hvr-underline-from-left brandonMed text-center blackBlue heigth-45"> <Button className="logout fz-12" style={{marginTop: '-5px'}} onClick={() => {
+		 				<Nav.Link onClick={close} href="/" className="hvr-underline-from-left text-center blackBlue heigth-45"> <Button className="logout fz-12" style={{marginTop: '-5px'}} onClick={() => {
 							sessionStorage.clear();
 		 					window.location.reload()
 		 				}}>Cerrar sesión</Button></Nav.Link>
