c9d99fcf1a31f4d3be24ac97d94d2412c99bcd50
wayasmiguel <wayasmiguel@outlook.com>
2020-06-15 00:13:56 -0500

    clip, comparativapopup

--- src/components/Generic/Chip.js
+++ src/components/Generic/Chip.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faIgloo,faPaperclip } from '@fortawesome/free-solid-svg-icons'
+import clip from "../../assets/img/clip-01.png";
 
 
 library.add(faIgloo,faPaperclip)
@@ -23,7 +24,8 @@ class Chip extends Component {
 		return ( 
 
 			<div className="chip">
-				<FontAwesomeIcon className="icon" icon="paperclip" style={{width : 96, height : 96}} />
+				<img className="icon" src={clip} style={{width : 96, height : 96}}style={{width : 96, height : 96}}/>
+				{/* <FontAwesomeIcon className="icon" icon="paperclip" style={{width : 96, height : 96}} /> */}
 						<label className="texto-chip">{this.props.fileName}</label>
 				<span className="closebtn" onClick={() => this.props.delete(this.props.index, this.props.docName)} >&times;</span>
 			</div>
