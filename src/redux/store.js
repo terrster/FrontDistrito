03b46d17a66bca0afdd1bf2b485e9105b4a2427a
wayasmiguel <wayasmiguel@outlook.com>
2020-07-23 17:08:09 -0500

    enable state only localhost

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,13 +2,6 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk'
 import reducer from './reducers';
 
-if(process.env.NODE_ENV === 'localhost'){
-
-}
-else{
-
-}
-
 const store = createStore(
     reducer,
     compose( applyMiddleware(thunk),
