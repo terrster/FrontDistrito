25c3c75cfad2a301f52932cc0a55729bde15f46d
wayasmiguel <wayasmiguel@outlook.com>
2020-07-23 17:04:58 -0500

    enable state only localhost

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,13 +2,21 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk'
 import reducer from './reducers';
 
+if(process.env.NODE_ENV === 'localhost'){
+
+}
+else{
+
+}
+
 const store = createStore(
     reducer,
     compose( applyMiddleware(thunk),
-
-        typeof window === 'object' &&
-            typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined' ?
-                window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+        // typeof window === 'object' &&
+        //     typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined' ?
+        //         window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
+        process.env.REACT_APP_CONFIGURATION === 'localhost' && 
+        window.devToolsExtension ? window.devToolsExtension() : f => f
     )
 );
 
