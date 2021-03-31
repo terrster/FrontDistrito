2eabb242c42e71f836a981eb9e9867271218c9e0
wayasmiguel <wayasmiguel@outlook.com>
2021-03-31 11:04:30 -0600

    feat[Brokers] - hide banner by owner id

--- src/components/Brokers/SignupBrokers.js
+++ src/components/Brokers/SignupBrokers.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-// import Title from "../Generic/Title";
+import Title from "../Generic/Title";
 import SignupFormBrokers from "../../forms/SignupFormBrokers";
 // import * as $ from "jquery";
 import "react-toastify/dist/ReactToastify.css";
@@ -77,27 +77,35 @@ const SignupBrokers = props => {
   else {
     return (
       <>
-      <Carousel id="brokers-carousel" className="mb-2" controls={false} indicators={false}>
-          <Carousel.Item>
-              <img className="d-block w-100"  src={imageBroker[versionImage]} alt="brokersBanner"/>
-              <Carousel.Caption>
-                <div className="btn-broker-container">
-                  <Button className={"btn-blue-brokers btn-broker"} onClick={() => history.push("/brokers")}>Aplica aquí</Button>
-                </div>
-              </Carousel.Caption>
-          </Carousel.Item>
-      </Carousel>
+      {
+        ownerId === '' &&
+          <Carousel id="brokers-carousel" className="mb-2" controls={false} indicators={false}>
+              <Carousel.Item>
+                  <img className="d-block w-100"  src={imageBroker[versionImage]} alt="brokersBanner"/>
+                  <Carousel.Caption>
+                    <div className="btn-broker-container">
+                      <Button className={"btn-blue-brokers btn-broker"} onClick={() => history.push("/brokers")}>Aplica aquí</Button>
+                    </div>
+                  </Carousel.Caption>
+              </Carousel.Item>
+          </Carousel>
+      }
       <div className="container mt-30">
         <Loader />
         {!props.statusSingup ? (
           <>
-            {/* <Title
-              className="fz56 text-center title-dp fw500"
-              title="¡Bienvenido a Distrito Pyme!"
-            />
-            <div className="mt-30 text-dp fw300 fz20 text-center mb-30">
-              <p className="gray50">Te ayudamos a crecer tu negocio <label className="blue-primary">&nbsp;#ComunidadDeCrédito&nbsp;</label></p>
-            </div> */}
+            {
+              ownerId !== '' &&
+              <>
+                <Title
+                  className="fz56 text-center title-dp fw500"
+                  title="¡Bienvenido a Distrito Pyme!"
+                />
+                <div className="mt-30 text-dp fw300 fz20 text-center mb-30">
+                  <p className="gray50">Te ayudamos a crecer tu negocio <label className="blue-primary">&nbsp;#ComunidadDeCrédito&nbsp;</label></p>
+                </div> 
+              </>
+            }
             <div className="mt-50">
 							<StepSignup/>
 						</div>
