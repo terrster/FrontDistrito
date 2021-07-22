7de0e9fe8be1ba3bc7b11216433cb4b58c60abbd
josecorzo <jose291@live.com.mx>
2021-07-21 21:04:54 -0500

    modificacion-tarjetas-req

--- src/components/Landing/HowWorks/HowWorks.js
+++ src/components/Landing/HowWorks/HowWorks.js
@@ -1,28 +1,77 @@
 import React from 'react';
 import Title from '../../Generic/Title';
-import { Row, Col} from 'react-bootstrap';
-import Icons from '../Icons/Icons';
+import { Row, Col, Card } from 'react-bootstrap';
 
-import REQUISITOS_BÁSICOS_01 from '../../../assets/img/REQUISITOS BÁSICOS-01.png';
-import REQUISITOS_BÁSICOS_02 from '../../../assets/img/REQUISITOS BÁSICOS-02.png';
-import REQUISITOS_BÁSICOS_03 from '../../../assets/img/REQUISITOS BÁSICOS-03.png';
+import '../../../css/HowWorks.css';
+
+
 
 const HowWorks = props => {
-	return(
-		<div id="howWorks" className="pt-4 text-center ml-auto mr-auto">
-			<Title title="Requisitos básicos" className="title-dp fw500 mb-1 fz42" />
-			<Title title="Fácil, rápido y sin papeleos" className="subtitle-dp fz32 fw300 ls-01" />
-				<Row className="justify-content-center mt-3">
-					<Col lg={3}>
-						<Icons img={REQUISITOS_BÁSICOS_01} alt="Simulador" width="100px" text="Ingresos mínimos de $20,000 pesos mensuales (comprobables)" />
-					</Col>
-					<Col lg={3}>
-						<Icons img={REQUISITOS_BÁSICOS_02} alt="Documentos" width="100px" text="Antigüedad del negocio mínima de 6 meses" />
-					</Col>
-					<Col lg={3} className="pt-2">
-						<Icons img={REQUISITOS_BÁSICOS_03} alt="Ofertas" width="100px" text="Buen buró (en caso de no contar con buen buró se puede analizar la opción de una garantía)"/>
-					</Col>
-				</Row>
+	return (
+		<div id="howWorks" className="pt-4 text-center ml-auto mr-auto mb-3">
+			<Title title="Requisitos básicos" className="title-dp fz42 fw300" />
+
+			<Title title="Para hacer aún más rápido tu proceso y autorización de crédito, ten a la mano tu RFC con el que facturas y clave CIEC*
+       <br/>*Opcional" className="subtitle-dp fz12 text-center mb-30 " />
+
+			<Row className="justify-content-center mt-3">
+				<Col xl={4} md={5} sm={6} xs={8} className="mb-4">
+					<Card style={{ height: '28rem' }} id="card-home">
+						<Card.Header>LOGO</Card.Header>
+						<Card.Body>
+							<Card.Title>Sin alta en el SAT</Card.Title>
+							<Card.Text className="metropolisReg fz12 blackBlue text-left mt-5">
+								<ul>
+									<li>Identificación Oficial</li>
+									<li className="mt-2">Comprobante de domicilio</li>
+									<li className="mt-2">Estados de cuenta bancarios</li>
+									<li className="mt-2">Mínimo 6 meses con tu negocio</li>
+									<li className="mt-2">Ventas mínimas de $15 mil pesos al mes</li>
+								</ul>
+							</Card.Text>
+						</Card.Body>
+					</Card>
+				</Col>
+
+				<Col xl={4} md={5} sm={6} xs={8} className="mb-4">
+					<Card style={{ height: '28rem' }} id="card-home">
+						<Card.Header>LOGO</Card.Header>
+						<Card.Body>
+							<Card.Title>Personas Físicas con Actividad Empresarial y RIF</Card.Title>
+							<Card.Text className="metropolisReg fz12 blackBlue text-left">
+								<ul>
+									<li className="mt-2">Identificación Oficial</li>
+									<li className="mt-2">RFC</li>
+									<li className="mt-2">Comprobante de domicilio</li>
+									<li className="mt-2">Estados de cuentas bancarios</li>
+									<li className="mt-2">Mínimo 12 meses con tu negocio</li>
+									<li className="mt-2">Ventas mínimas de $25 mil al mes</li>
+								</ul>
+							</Card.Text>
+						</Card.Body>
+					</Card>
+				</Col>
+
+				<Col xl={4} md={5} sm={6} xs={8} className="mb-4">
+					<Card style={{ height: '28rem' }} id="card-home">
+						<Card.Header>LOGO</Card.Header>
+						<Card.Body>
+							<Card.Title>Persona Moral</Card.Title>
+							<Card.Text className="metropolisReg fz12 blackBlue text-left mt-5">
+								<ul>
+									<li>Identificación Oficial principal accionista y representante legal</li>
+									<li className="mt-2">RFC</li>
+									<li className="mt-2">Comprobante de domicilio del negocio y personal</li>
+									<li className="mt-2">Estados de cuentas bancarios</li>
+									<li className="mt-2">Acta constitutiva</li>
+									<li className="mt-2">Mínimo 12 meses con tu negocio</li>
+									<li className="mt-2">Ventas mínimas de $50 mil al mes</li>
+								</ul>
+							</Card.Text>
+						</Card.Body>
+					</Card>
+				</Col>
+			</Row>
 		</div>
 	)
 }
