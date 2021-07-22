530ccb1987fbc9f857756ded1739d09539e3a396
josecorzo <jose291@live.com.mx>
2021-07-21 21:43:40 -0500

    modificacion-tarjetas-op

--- src/components/Landing/CreditOption/CreditOption.js
+++ src/components/Landing/CreditOption/CreditOption.js
@@ -20,10 +20,10 @@ const CreditOption = () => {
         <Row className="justify-content-center mt-3">
           <Col xl={4} md={5} sm={6} xs={8} className="mb-4">
             <Card id="card-credit" className="fondo-card" style={{ height: '26.3rem' }}>
-              <Card.Header id="header" style={{ height: '5rem' }} className="title-card-home-dp fz25">Bancos, financieras y fintechs</Card.Header>
+              <Card.Header id="header" style={{ height: '5rem' }} className="fz25">Bancos, financieras y fintechs</Card.Header>
               <Card.Body>
                 <Card.Title></Card.Title>
-                <Card.Text className="metropolisReg fz12 blackBlue text-left">
+                <Card.Text className="text-dp fz12  text-left">
                   <ul className="bancos-list">
                     <li>Tienes que ir de sucursal en sucursal o de página en página para ver quien te presta</li>
                     <li className="mt-2">Trámites largos y complicados</li>
@@ -40,7 +40,7 @@ const CreditOption = () => {
               <Card.Header id="header" style={{ height: '5rem' }}><img src={LOGODP} alt="LOGODP" width="100px" /></Card.Header>
               <Card.Body>
                 <Card.Title></Card.Title>
-                <Card.Text className="metropolisReg fz12 white text-left ">
+                <Card.Text className="text-dp fz12 white text-left ">
                   <ul className="dp-list">
                     <li className="dp-opciones">Con una sola solicitud te ofrecemos las mejores opciones de crédito acorde a las características
                       de tu negocio
