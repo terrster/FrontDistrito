5e5755f83de16d38f02004361119d4139a5ab071
wayasmiguel <wayasmiguel@outlook.com>
2021-07-22 11:52:05 -0500

    backing to bd190e04

--- src/components/Generic/Title.js
+++ src/components/Generic/Title.js
@@ -2,11 +2,10 @@ import React from 'react';
 
 const Title = props => {
 	return (
-		<div dangerouslySetInnerHTML={{ __html: props.title }} className={props.className}>
-			{/* {props.title} */}
+		<div className={props.className}>
+			{props.title}
 		</div>
 	);
 }
 
-
-export default Title;
+export default Title
